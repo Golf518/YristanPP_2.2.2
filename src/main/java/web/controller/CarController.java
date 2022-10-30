0e828bb48b1e9ef092c4f17a23e29982c452192d
yushayypbek <uristan92@gmail.com>
2022-10-30 20:57:12 +0600

    commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,19 +1,34 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarServiceImp;
 
+import java.util.stream.Collectors;
+
 @Controller
 public class CarController {
+    @Autowired
+    private CarServiceImp carServiceImp;
+    public CarController(CarServiceImp carServiceImp) {
+        this.carServiceImp = carServiceImp;
+    }
+
 
 
     @GetMapping(value = "/cars")
-    public String printCars(@RequestParam(defaultValue = "5")Integer count, Model model) {
-        model.addAttribute("cars",
-                new CarServiceImp().returnQtyOfCar(count));
-        return "car";
+    public String printCars(@RequestParam(value = "count", required = false)Integer count, ModelMap model) {
+        if (count!=null) {
+            model.addAttribute("cars",
+                    carServiceImp.returnQtyOfCar(count).stream().limit(count).collect(Collectors.toList()));
+        } else {
+            model.addAttribute("cars", carServiceImp.carList());
+        }
+        return "cars";
+
     }
 }
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,25 +1,32 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+@Component
+public class CarServiceImp {
+    private  List<Car> cars;
 
-public class CarServiceImp implements CarService{
-    @Override
-    public List<Car> addCarList() {
-        List<Car> cars = new ArrayList<>();
+    {
+        cars = new ArrayList<>();
         cars.add(new Car("BMW", 34, "black"));
         cars.add(new Car("MB", 124,"silver"));
         cars.add(new Car("Honda", 9, "white" ));
         cars.add(new Car("Mitsubishi", 154, "red"));
         cars.add(new Car("MB", 210,"red") );
+
+    }
+    public List<Car> carList() {
         return cars;
     }
 
-    @Override
+
     public List<Car> returnQtyOfCar(Integer count) {
-        return addCarList().stream().limit(count < 0 ? 0 : count).collect(Collectors.toList());
+        carList().stream().limit(count).collect(Collectors.toList());
+
+        return cars;
     }
 }
